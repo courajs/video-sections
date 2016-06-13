086a41c9b0d753f3d7fddf0ca0781fcdcab35a4e
Aaron Sikes <aaron@sikes.io>
2016-06-13 15:40:55 -0400

    Set up play/pause events

--- src/video-sections.js
+++ src/video-sections.js
@@ -1,10 +1,15 @@
 xtag.register('video-sections', {
   lifecycle: {
     created() {
+      this._onPlay = this.onPlay.bind(this);
+      this._onPause = this.onPause.bind(this);
+
       this.video = document.createElement('video');
       this.video.controls = true;
       this.video.muted = true;
       this.appendChild(this.video);
+
+      this.registerListeners();
     }
   },
   accessors: {
@@ -16,6 +21,22 @@ xtag.register('video-sections', {
       set(src) {
         this.video.src = src;
       }
+    }
+  },
+  methods: {
+    registerListeners() {
+      this.video.addEventListener('play', this._onPlay);
+      this.video.addEventListener('pause', this._onPause);
+    },
+    clearListeners() {
+      this.video.removeEventListener('play', this._onPlay);
+      this.video.removeEventListener('pause', this._onPause);
+    },
+    onPlay() {
+      console.log("playing");
+    },
+    onPause() {
+      console.log("paused");
     }
   }
 });

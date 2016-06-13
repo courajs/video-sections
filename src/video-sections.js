56498fc7b4941ce360ffd518abc2a22cb2ed92e9
Aaron Sikes <aaron@sikes.io>
2016-06-13 15:36:54 -0400

    Proxy the src attribute to an internal video element

--- src/video-sections.js
+++ src/video-sections.js
@@ -1,3 +1,21 @@
 xtag.register('video-sections', {
-  content: 'hey'
+  lifecycle: {
+    created() {
+      this.video = document.createElement('video');
+      this.video.controls = true;
+      this.video.muted = true;
+      this.appendChild(this.video);
+    }
+  },
+  accessors: {
+    src: {
+      attribute: {},
+      get() {
+        return this.video.src;
+      },
+      set(src) {
+        this.video.src = src;
+      }
+    }
+  }
 });
